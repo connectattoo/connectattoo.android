4ecfe6f208dc02529673a8d7b6f4c13c297f2773
Sérgio <sergioriosribeiro@outlook.com>
2024-06-14 17:56:11 -0300

    feat: creates UserSearchViewHolder class

---
+++ app/src/main/java/br/com/connectattoo/adapter/UserSearchViewHolder.kt
@@ -0,0 +1,7 @@
+package br.com.connectattoo.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class UserSearchViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+}

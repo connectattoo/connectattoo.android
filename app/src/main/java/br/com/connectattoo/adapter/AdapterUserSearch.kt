8c602783db6dba566682a4b345c4f8affb112eff
Sérgio <sergioriosribeiro@outlook.com>
2024-06-14 17:55:04 -0300

    feat: creates the AdapterUserSearch class

---
+++ app/src/main/java/br/com/connectattoo/adapter/AdapterUserSearch.kt
@@ -0,0 +1,19 @@
+package br.com.connectattoo.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Recycler
+
+class AdapterUserSearch : RecyclerView.Adapter<UserSearchViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserSearchViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: UserSearchViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}

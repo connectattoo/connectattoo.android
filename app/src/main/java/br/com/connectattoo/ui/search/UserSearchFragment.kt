5adf1ff1d366511cc22f902399aa3df46ec33297
Sérgio <sergioriosribeiro@outlook.com>
2024-07-15 19:33:14 -0300

    feat: creates fun observe and fun getRandomTattoos to deal with the sent results from UserSearchViewModel

--- app/src/main/java/br/com/connectattoo/ui/search/UserSearchFragment.kt
+++ app/src/main/java/br/com/connectattoo/ui/search/UserSearchFragment.kt
@@ -5,15 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import br.com.connectattoo.adapter.AdapterUserSearch
 import br.com.connectattoo.databinding.FragmentUserSearchBinding
+import br.com.connectattoo.repository.UserSearchRepository
 import br.com.connectattoo.ui.BaseFragment
+import br.com.connectattoo.utils.Constants
+import br.com.connectattoo.utils.DataStoreManager
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.launch
 
 class UserSearchFragment : BaseFragment<FragmentUserSearchBinding>() {
     private lateinit var viewModel: UserSearchViewModel
+    private var repository: UserSearchRepository = UserSearchRepository()
+    private val recyclerSearch = binding.recyclerImagesSearch
+    private lateinit var adapter: AdapterUserSearch
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -23,10 +31,11 @@ class UserSearchFragment : BaseFragment<FragmentUserSearchBinding>() {
         val searchView = binding.fragmentUserSearch
         searchView.visibility = View.VISIBLE
 
-        val recyclerSearch = binding.recyclerImagesSearch
-        recyclerSearch.layoutManager = StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL)
-        recyclerSearch.adapter = AdapterUserSearch()
-
+        recyclerSearch.layoutManager =
+            StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL)
+            recyclerSearch.adapter = adapter
+        observe()
+        getRandomTattoos()
     }
 
     override fun inflateBinding(
@@ -37,5 +46,17 @@ class UserSearchFragment : BaseFragment<FragmentUserSearchBinding>() {
         return FragmentUserSearchBinding.inflate(inflater, container, false)
     }
 
+    private fun getRandomTattoos() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            val token = DataStoreManager.getUserSettings(requireContext(), Constants.API_TOKEN)
+            viewModel.getRandomTattoos(repository, token)
+        }
+    }
 
+    private fun observe() {
+        viewModel.imagesTattooUserSearch.observe(this) {
+            adapter.updateTattooImages(it)
+        }
+        viewModel.message.observe(this) { Snackbar.make(requireView(), it, Snackbar.LENGTH_SHORT) }
+    }
 }
